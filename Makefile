f22cd50c442dd8232fa62f700ca57bcf18de79e1
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-08-26 16:16:36 +0200

    Made files modifications

--- Makefile
+++ Makefile
@@ -1,14 +1,27 @@
-# env: 
-# 	python3 -m venv myrepo && source myrepo/bin/activate
-
 install:
-	pip install resolvelib && pip install --upgrade pip && pip install -r requirements.txt
-
-lint:
-	pylint --disable=R,C,W0012,W1203,W0702,E0611,W0611,C0103,C0114,C0116,C0209 app.py
-
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+	
 test:
 	python -m pytest -vv test_hello.py
 
-all:	install test lint
+lint:
+		pylint --disable=R,C hello.py
+
+all: install lint test
+
+# env: 
+# 	python3 -m venv myrepo && source myrepo/bin/activate
+
+# install:
+# 	pip install resolvelib && pip install --upgrade pip && pip install -r requirements.txt
+
+# lint:
+# 	pylint --disable=R,C,W0012,W1203,W0702,E0611,W0611,C0103,C0114,C0116,C0209 app.py
+
+# test:
+# 	python -m pytest -vv test_hello.py
+
+# all:	install test lint
+
 
--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, between, task
 
 class WebsiteUser(HttpUser):
-    host = "https://<yourappname>.azurewebsites.net:443"
+    host = "https://myflaskmlwebappy.azurewebsites.net:443"
     wait_time = between(5, 15)
 
     @task
@@ -30,3 +30,37 @@ class WebsiteUser(HttpUser):
                 "0":4.98
             }
         })
+
+# from locust import HttpUser, between, task
+
+# class WebsiteUser(HttpUser):
+#     host = "https://<yourappname>.azurewebsites.net:443"
+#     wait_time = between(5, 15)
+
+#     @task
+#     def index(self):
+#         self.client.get("/")
+
+#     @task
+#     def predict(self):
+#         self.client.post("/predict", json={
+#             "CHAS":{
+#                 "0":0
+#             },
+#             "RM":{
+#                 "0":6.575
+#             },
+#             "TAX":{
+#                 "0":296.0
+#             },
+#             "PTRATIO":{
+#                 "0":15.3
+#             },
+#             "B":{
+#                 "0":396.9
+#             },
+#             "LSTAT":{
+#                 "0":4.98
+#             }
+#         })
+
